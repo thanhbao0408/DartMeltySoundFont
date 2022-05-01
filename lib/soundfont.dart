fea6d9a26c3c2baf6f3da285842e9e3038c6fa5f
Ahmet Uzun <au250073@ncr.com>
2022-05-01 11:44:46 +0300

    fixed bug in soundfont _checkSamples

--- lib/soundfont.dart
+++ lib/soundfont.dart
@@ -1,5 +1,4 @@
-﻿
-import 'dart:typed_data';
+﻿import 'dart:typed_data';
 
 import 'instrument_region.dart';
 import 'array_int16.dart';
@@ -38,21 +37,18 @@ class SoundFont {
 
   /// Loads a SoundFont from the file.
   factory SoundFont.fromFile(String path) {
-    
     BinaryReader reader = BinaryReader.fromFile(path);
 
     return SoundFont.fromBinaryReader(reader);
   }
 
   factory SoundFont.fromByteData(ByteData bytes) {
-    
     BinaryReader reader = BinaryReader.fromByteData(bytes);
 
     return SoundFont.fromBinaryReader(reader);
   }
 
   factory SoundFont.fromBinaryReader(BinaryReader reader) {
-
     String chunkId = reader.readFourCC();
     if (chunkId != "RIFF") {
       throw "The RIFF chunk was not found.";
@@ -92,12 +88,10 @@ class SoundFont {
   }
 
   void _checkSamples() {
-
     // This offset is to ensure that out of range access is safe.
     var sampleCount = waveData.bytes.lengthInBytes - 4;
 
     for (SampleHeader sample in sampleHeaders) {
-
       if (!(0 <= sample.start && sample.start < sampleCount)) {
         throw "The start position of the sample '${sample.name}' is out of range.";
       }
@@ -110,20 +104,18 @@ class SoundFont {
         throw "The end position of the sample '${sample.name}' is out of range.";
       }
 
-      if (!(0 < sample.endLoop && sample.endLoop <= sampleCount)) {
+      if (!(0 <= sample.endLoop && sample.endLoop <= sampleCount)) {
         throw "The loop end position of the sample '${sample.name}' is out of range.";
       }
     }
   }
 
   void _checkRegions() {
-    
     // This offset is to ensure that out of range access is safe.
     var sampleCount = waveData.bytes.lengthInBytes - 4;
 
     for (Instrument instrument in instruments) {
       for (InstrumentRegion region in instrument.regions) {
-
         if (!(0 <= region.sampleStart() &&
             region.sampleStart() < sampleCount)) {
           throw "'sampleStart' is out of range. '${region.sample.name}'.'${instrument.name}'.";
@@ -137,7 +129,7 @@ class SoundFont {
         if (!(0 < region.sampleEnd() && region.sampleEnd() <= sampleCount)) {
           throw "'sampleEnd' is out of range. '${region.sample.name}'.'${instrument.name}'.";
         }
-        
+
         if (!(0 < region.sampleEndLoop() &&
             region.sampleEndLoop() <= sampleCount)) {
           throw "'sampleEndLoop' is out of range. '${region.sample.name}'.'${instrument.name}'.";
